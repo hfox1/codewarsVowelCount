fc746e9bbf8410aed20e05261de1076a182f81cb
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-12-08 18:06:47 +0100

    pattern abstracted, tests passing

--- getCount.js
+++ getCount.js
@@ -1,13 +1,5 @@
 const getCount = (str) => {
-	if (str == 'abracadabra') {
-		return 5;
-	}
-	if (str == 'test') {
-		return 1;
-	}
-	if (str == 'mood') {
-		return 2;
-	}
+	return str.match(/[aeiou]/g).length;
 };
 
 module.exports = getCount;
