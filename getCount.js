8c8c4bb76d019464211ae0b50046769086198b20
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-12-10 11:48:16 +0100

    added null protection

--- getCount.js
+++ getCount.js
@@ -1,5 +1,5 @@
 const getCount = (str) => {
-	return str.match(/[aeiou]/g).length;
+	return str.match(/[aeiou]/g) ? str.match(/[aeiou]/g).length : 0;
 };
 
 module.exports = getCount;
--- getCount.test.js
+++ getCount.test.js
@@ -7,7 +7,13 @@ describe('Vowels Count Tests', () => {
 	it("should return 1 for 'test'", () => {
 		expect(getCount('test')).toEqual(1);
 	});
-	it("should return 5 for 'mood'", () => {
+	it("should return 2 for 'mood'", () => {
 		expect(getCount('mood')).toEqual(2);
+	});
+	it("should return 2 for 'made'", () => {
+		expect(getCount('made')).toEqual(2);
+	});
+	it("should return 0 for 'pry'", () => {
+		expect(getCount('pry')).toEqual(0);
 	});
 });
